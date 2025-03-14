101e4244ec86516ddc2bdc2f21222e2825c790be
zenga123 <zenga85@naver.com>
2025-03-14 18:41:29 +0900

    Initial Commit

---
+++ project_tokei_IPhone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project_tokei_IPhone
+//
+//  Created by musung on 2025/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project_tokei_IPhone/project_tokei_IPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_tokei_IPhoneApp.swift
+//  project_tokei_IPhone
+//
+//  Created by musung on 2025/03/14.
+//
+
+import SwiftUI
+
+@main
+struct project_tokei_IPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
